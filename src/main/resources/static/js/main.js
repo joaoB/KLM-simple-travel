5c409d95f09821153e3a60a21eef2484501e6ef1
joaoB <silva.joaobruno@gmail.com>
2016-08-23 14:18:36 -0600

    front end integration with select

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -52,11 +52,57 @@ $(document)
 								});
 					}
 
-					$('#from').keyup(function() {
+					/*$('#from').keyup(function() {
 						getByTerm($(this).val())
 
 						console.log("make request to api and parse json")
 
+					});*/
+					
+					$('#from').selectize({
+					    valueField: 'title',
+					    labelField: 'title',
+					    searchField: 'title',
+					    options: [],
+					    create: false,
+					    render: {
+					        option: function(item, escape) {
+					            var actors = [];
+					            for (var i = 0, n = item.abridged_cast.length; i < n; i++) {
+					                actors.push('<span>' + escape(item.abridged_cast[i].name) + '</span>');
+					            }
+
+					            return '<div>' +
+					                '<img src="' + escape(item.posters.thumbnail) + '" alt="">' +
+					                '<span class="title">' +
+					                    '<span class="name">' + escape(item.title) + '</span>' +
+					                '</span>' +
+					                '<span class="description">' + escape(item.synopsis || 'No synopsis available at this time.') + '</span>' +
+					                '<span class="actors">' + (actors.length ? 'Starring ' + actors.join(', ') : 'Actors unavailable') + '</span>' +
+					            '</div>';
+					        }
+					    },
+					    load: function(query, callback) {
+					        if (!query.length) return callback();
+					        $.ajax({
+					            url: 'http://localhost:8080/airports',
+					            type: 'GET',
+					            
+					            data: {
+					                term: query
+					            },
+					            error: function() {
+					                callback();
+					            },
+					            success: function(res) {
+					                callback(res.movies);
+					            },
+								beforeSend : function(xhr, settings) {
+									xhr.setRequestHeader('Authorization', 'Bearer '
+											+ token);
+								}
+					        });
+					    }
 					});
 
 					getToken();
